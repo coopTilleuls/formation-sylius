3262d9dbdd7fed1f7021e436ace08de61c968a7f
Arnaud Frézet <arnaud@les-tilleuls.coop>
2021-11-26 14:46:37 +0100

    feature: move Sylius Doctrine entity annotations to php attributes

--- src/Entity/User/AdminUser.php
+++ src/Entity/User/AdminUser.php
@@ -7,10 +7,12 @@ namespace App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\AdminUser as BaseAdminUser;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_admin_user")
- */
+#[ORM\Entity]
+#[ORM\Table(name: 'sylius_admin_user')]
 class AdminUser extends BaseAdminUser
 {
+    public function getUserIdentifier(): string
+    {
+        return $this->getUsername();
+    }
 }
